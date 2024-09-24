de0487a946041dc082567214e546b3cea8cb1fdf
Andrea Di Claudio <andreadiclaudio@Mac.home>
2024-09-24 11:24:16 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nosql_project
+
+# NoSQL Project
+OLA for NoSQL Project by Orestis, Sugat, Andrea
+
+# How to install
+
+1. Clone GitHub repository.
+2. Run `npm i`.
+3. Create a `.env` file and set the `PORT=` to the desired port.
+4. Run `node app.js`.
---
+++ app.js
@@ -0,0 +1,22 @@
+// 'use strict' enforces stricter parsing and error handling, helping.
+'use strict';
+
+import express from "express";
+import dotenv from "dotenv";
+
+// Import the express module to create a web server
+const app = express();
+// Load environment variables from a .env file into process.env
+dotenv.config();
+
+// Test get request
+app.get('/api/test', (req, res) => {
+    res.status(200).json({message: 'Hello World'});
+});
+
+// Define the port on which the server will listen, using an environment variable
+const PORT = process.env.PORT;
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log("Server is running on port:", PORT);
+});
